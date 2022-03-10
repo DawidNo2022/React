cb10e24f2579ed770acbb6ba2c2e170a569b2a9f
Dawid Skarwecki <dawidskarwecki@gmail.com>
2022-03-10 10:59:27 +0100

    Add useSelector

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,13 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
+import { useSelector } from 'react-redux';
 
-const Column = ({ id, title, icon, cards, addCard }) => {
+const Column = ({ id, title, icon, addCard }) => {
   //przerobic propsy w reszcie komp
+  const cards = useSelector((state) => state.cards).filter(
+    (card) => card.columnId === id
+  );
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,4 @@
-import { useState } from 'react'; //miedzy wlasnymi
-import shortid from 'shortid';
+import { useSelector } from 'react-redux';
 
 import styles from './List.module.scss'; //style ostatnie
 import Column from '../Column/Column.js';
@@ -7,58 +6,7 @@ import ColumnForm from '../ColumnForm/ColumnForm.js';
 
 const List = () => {
   //funkcja strzalkowa
-  const [columns, setColumns] = useState([
-    {
-      id: 1,
-      title: 'Books',
-      icon: 'book',
-      cards: [
-        { id: 1, title: 'This is Going to Hurt' },
-        { id: 2, title: 'Interpreter of Maladies' },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Movies',
-      icon: 'film',
-      cards: [
-        { id: 1, title: 'Harry Potter' },
-        { id: 2, title: 'Star Wars' },
-      ],
-    },
-    {
-      id: 3,
-      title: 'Games',
-      icon: 'gamepad',
-      cards: [
-        { id: 1, title: 'The Witcher' },
-        { id: 2, title: 'Skyrim' },
-      ],
-    },
-  ]);
-  const addCard = (newCard, columnId) => {
-    const columnsUpdated = columns.map((column) => {
-      if (column.id === columnId)
-        return {
-          ...column,
-          cards: [...column.cards, { id: shortid(), title: newCard.title }],
-        };
-      else return column;
-    });
-
-    setColumns(columnsUpdated);
-  };
-  const addColumn = (newColumn) => {
-    setColumns([
-      ...columns,
-      {
-        id: shortid(),
-        title: newColumn.title,
-        icon: newColumn.icon,
-        cards: [],
-      }, //przecinki
-    ]);
-  };
+  const columns = useSelector((state) => state.columns);
 
   return (
     //co ma zwracac dany komponent
@@ -75,15 +23,11 @@ const List = () => {
         {columns.map((column) => (
           <Column
             key={column.id}
-            id={column.id}
-            title={column.title}
-            icon={column.icon}
-            cards={column.cards}
-            addCard={addCard}
+            {...column} //spread operator
           /> //key nie uzywac indexu array
         ))}
       </section>
-      <ColumnForm action={addColumn} />
+      <ColumnForm />
     </div>
   );
 };
